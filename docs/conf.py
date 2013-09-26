6cc62f83d66376d774ed537ac7af1a389342a7d3
Ron Frederick <ronf@timeheart.net>
2013-09-25 19:22:57 -0700

    Widen the left sidebar to avoid wrapping a line in the table of contents

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,7 @@ html_theme = 'rftheme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "sidebarwidth": 270,
+    "sidebarwidth": 285,
     "stickysidebar": "true"
 }
 
