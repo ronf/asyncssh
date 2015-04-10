3408c53f49cb64ff20fbc659ab960a024f6d7470
Ron Frederick <ronf@timeheart.net>
2015-04-09 22:00:05 -0700

    Update to Sphinx 1.3
    This commit updates AsyncSSH to use Sphinx 1.3, switching from the
    HTML theme name from 'default' to 'classic'. It also enables intersphinx
    to allow linking to the base Python documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ from asyncssh import __author__, __version__
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -241,3 +241,5 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
