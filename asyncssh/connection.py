ronf/asyncssh

commit d356937db8dd591f08fec7a3538d961bfed09c04

Finish off public key unit tests
This commit adds unit tests for the SSH certificate support, and a few
other previously uncovered places in the public_key module.

To properly generate test certificates, the encode_ssh_public() method
in the SSH key classes has been refactored to be symmetric with the
decode_ssh_public() function, not automatically adding in the key
algorithm. This is now done in the calling code where it is needed.

In addition, a bug was fixed when a passphrase was passed as already
encoded bytes, rather than as a string, and unit tests were added to
cover both cases. Passing in bytes can be tricky, though, as the proper
encoding of those bytes will vary depending on the cipher and PBE
version selected.

A handful of lines were also explicitly marked for exclusion from
coverage, as they would only be executed when some of the supporting
modules are not installed.

asyncssh/public_key.py
@@ -20,7 +20,7 @@ from os import urandom
 try:
     import bcrypt
     _bcrypt_available = True
-except ImportError:
+except ImportError: # pragma: no cover
     _bcrypt_available = False
 
 from .asn1 import ASN1DecodeError, BitString, der_encode, der_decode
@@ -223,11 +223,12 @@ class SSHKey:
             nkeys = 1
             comment = b''
 
-            data = b''.join((check, check, self.encode_ssh_private(),
-                             String(comment)))
+            keydata = String(self.algorithm) + self.encode_ssh_private()
+
+            data = b''.join((check, check, keydata, String(comment)))
 
             if passphrase is not None:
-                if not _bcrypt_available:
+                if not _bcrypt_available: # pragma: no cover
                     raise KeyExportError('OpenSSH private key encryption '
                                          'requires bcrypt')
 
@@ -243,9 +244,11 @@ class SSHKey:
                 salt = urandom(_OPENSSH_SALT_LEN)
                 kdf_data = b''.join((String(salt), UInt32(rounds)))
 
+                if isinstance(passphrase, str):
+                    passphrase = passphrase.encode('utf-8')
+
                 # pylint: disable=no-member
-                key = bcrypt.kdf(passphrase.encode('utf-8'), salt,
-                                 key_size + iv_size, rounds)
+                key = bcrypt.kdf(passphrase, salt, key_size + iv_size, rounds)
                 # pylint: enable=no-member
 
                 cipher = get_cipher(alg, key[:key_size], key[key_size:])
@@ -259,7 +262,7 @@ class SSHKey:
                 mac = b''
 
             pad = len(data) % block_size
-            if pad:
+            if pad: # pragma: no branch
                 data = data + bytes(range(1, block_size + 1 - pad))
 
             if cipher:
@@ -270,9 +273,10 @@ class SSHKey:
                 else:
                     data, mac = cipher.encrypt(data), b''
 
+            keydata = String(self.algorithm) + self.encode_ssh_public()
+
             data = b''.join((_OPENSSH_KEY_V1, String(alg), String(kdf),
-                             String(kdf_data), UInt32(nkeys),
-                             String(self.encode_ssh_public()),
+                             String(kdf_data), UInt32(nkeys), String(keydata),
                              String(data), mac))
 
             return (b'-----BEGIN OPENSSH PRIVATE KEY-----\n' +
@@ -316,12 +320,14 @@ class SSHKey:
 
             return data
         elif format_name == 'openssh':
-            data = self.encode_ssh_public()
+            data = String(self.algorithm) + self.encode_ssh_public()
 
             return self.algorithm + b' ' + binascii.b2a_base64(data)
         elif format_name == 'rfc4716':
+            data = String(self.algorithm) + self.encode_ssh_public()
+
             return (b'---- BEGIN SSH2 PUBLIC KEY ----\n' +
-                    _wrap_base64(self.encode_ssh_public()) +
+                    _wrap_base64(data) +
                     b'---- END SSH2 PUBLIC KEY ----\n')
         else:
             raise KeyExportError('Unknown export format')
@@ -646,7 +652,7 @@ def _decode_openssh_private(data, passphrase):
             raise KeyImportError('Invalid OpenSSH private key')
 
         if cipher_name != b'none':
-            if not _bcrypt_available:
+            if not _bcrypt_available: # pragma: no cover
                 raise KeyEncryptionError('OpenSSH private key encryption '
                                          'requires bcrypt')
 
@@ -670,14 +676,16 @@ def _decode_openssh_private(data, passphrase):
             rounds = packet.get_uint32()
             packet.check_end()
 
-            # pylint: disable=no-member
+            if isinstance(passphrase, str):
+                passphrase = passphrase.encode('utf-8')
+
             try:
-                key = bcrypt.kdf(passphrase.encode('utf-8'), salt,
-                                 key_size + iv_size, rounds)
+                # pylint: disable=no-member
+                key = bcrypt.kdf(passphrase, salt, key_size + iv_size, rounds)
+                # pylint: enable=no-member
             except ValueError:
                 raise KeyEncryptionError('Invalid OpenSSH '
                                          'private key') from None
-            # pylint: enable=no-member
 
             cipher = get_cipher(cipher_name, key[:key_size], key[key_size:])
 

asyncssh/dsa.py
@@ -173,16 +173,15 @@ class _DSAKey(SSHKey):
         if not self._private:
             raise KeyExportError('Key is not private')
 
-        return b''.join((String(self.algorithm), MPInt(self._key.p),
-                         MPInt(self._key.q), MPInt(self._key.g),
-                         MPInt(self._key.y), MPInt(self._key.x)))
+        return b''.join((MPInt(self._key.p), MPInt(self._key.q),
+                         MPInt(self._key.g), MPInt(self._key.y),
+                         MPInt(self._key.x)))
 
     def encode_ssh_public(self):
         """Encode an SSH format DSA public key"""
 
-        return b''.join((String(self.algorithm), MPInt(self._key.p),
-                         MPInt(self._key.q), MPInt(self._key.g),
-                         MPInt(self._key.y)))
+        return b''.join((MPInt(self._key.p), MPInt(self._key.q),
+                         MPInt(self._key.g), MPInt(self._key.y)))
 
     def sign(self, data):
         """Return a signature of the specified data using this key"""

asyncssh/ec.py
@@ -532,14 +532,13 @@ class _ECKey(SSHKey):
         if not self._d:
             raise KeyExportError('Key is not private')
 
-        return b''.join((String(self.algorithm), String(self._alg_id),
-                         String(self._Q.encode()), MPInt(self._d)))
+        return b''.join((String(self._alg_id), String(self._Q.encode()),
+                         MPInt(self._d)))
 
     def encode_ssh_public(self):
         """Encode an SSH format EC public key"""
 
-        return b''.join((String(self.algorithm), String(self._alg_id),
-                         String(self._Q.encode())))
+        return b''.join((String(self._alg_id), String(self._Q.encode())))
 
     def sign(self, data):
         """Return a signature of the specified data using this key"""

asyncssh/ed25519.py
@@ -74,13 +74,12 @@ class _Ed25519Key(SSHKey):
         if self._sk is None:
             raise KeyExportError('Key is not private')
 
-        return b''.join((String(self.algorithm), String(self._vk),
-                         String(self._sk)))
+        return b''.join((String(self._vk), String(self._sk)))
 
     def encode_ssh_public(self):
         """Encode an SSH format Ed25519 public key"""
 
-        return b''.join((String(self.algorithm), String(self._vk)))
+        return String(self._vk)
 
     def sign(self, data):
         """Return a signature of the specified data using this key"""
@@ -110,7 +109,7 @@ class _Ed25519Key(SSHKey):
 
 try:
     import libnacl
-except (ImportError, OSError):
+except (ImportError, OSError): # pragma: no cover
     pass
 else:
     register_public_key_alg(b'ssh-ed25519', _Ed25519Key)

asyncssh/rsa.py
@@ -166,16 +166,15 @@ class _RSAKey(SSHKey):
         if not self._private:
             raise KeyExportError('Key is not private')
 
-        return b''.join((String(self.algorithm), MPInt(self._key.n),
-                         MPInt(self._key.e), MPInt(self._key.d),
+        return b''.join((MPInt(self._key.n), MPInt(self._key.e),
+                         MPInt(self._key.d),
                          MPInt(mod_inverse(self._key.q, self._key.p)),
                          MPInt(self._key.p), MPInt(self._key.q)))
 
     def encode_ssh_public(self):
         """Encode an SSH format RSA public key"""
 
-        return b''.join((String(self.algorithm), MPInt(self._key.e),
-                         MPInt(self._key.n)))
+        return b''.join((MPInt(self._key.e), MPInt(self._key.n)))
 
     def sign(self, data):
         """Return a signature of the specified data using this key"""

tests/.coveragerc
@@ -1,0 +1,7 @@
+[run]
+branch = True
+
+[report]
+exclude_lines =
+    pragma: no cover
+    raise NotImplementedError

commit 4fecf50fe2f4a83c92b65cc9fafca9354ff7c544

Fix bug introduced in public key encoding by commit d356937
This commit fixes a bug introduced in commit d356937 related to
encoding SSH public key blobs. When encode_ssh_public() was changed
to not encode the algorithm, some calls to it were missed. This new
implementation adds a new function get_ssh_public_key() which should
be used to get an encoded binary SSH public key blob and uses
encode_ssh_public() only internally within the SSHKey class.

asyncssh/connection.py
@@ -159,7 +159,7 @@ def _load_private_key(key):
     elif isinstance(cert, bytes):
         cert = import_certificate(cert)
 
-    if cert and key.encode_ssh_public() != cert.key.encode_ssh_public():
+    if cert and key.get_ssh_public_key() != cert.key.get_ssh_public_key():
         raise ValueError('Certificate key mismatch')
 
     return key, cert
@@ -1873,7 +1873,7 @@ class SSHClientConnection(SSHConnection):
             self._client_keys.insert(0, (key, None))
             return cert.algorithm, key, cert.data
         elif key:
-            return key.algorithm, key, key.encode_ssh_public()
+            return key.algorithm, key, key.get_ssh_public_key()
         else:
             return None, None, None
 
@@ -2481,7 +2481,7 @@ class SSHServerConnection(SSHConnection):
                                  key.algorithm.decode('ascii'))
 
             self._server_host_keys[key.algorithm] = (key,
-                                                     key.encode_ssh_public())
+                                                     key.get_ssh_public_key())
 
             if cert:
                 if cert.algorithm in self._server_host_keys:

asyncssh/public_key.py
@@ -116,6 +116,11 @@ class SSHKey:
         # pylint: disable=no-self-use
         raise KeyExportError('OpenSSH public key export not supported')
 
+    def get_ssh_public_key(self):
+        """Return OpenSSH public key in binary format"""
+
+        return String(self.algorithm) + self.encode_ssh_public()
+
     def export_private_key(self, format_name, passphrase=None,
                            cipher_name='aes256-cbc', hash_name='sha256',
                            pbe_version=2, rounds=16):
@@ -271,10 +276,9 @@ class SSHKey:
                 else:
                     data, mac = cipher.encrypt(data), b''
 
-            keydata = String(self.algorithm) + self.encode_ssh_public()
-
             data = b''.join((_OPENSSH_KEY_V1, String(alg), String(kdf),
-                             String(kdf_data), UInt32(nkeys), String(keydata),
+                             String(kdf_data), UInt32(nkeys),
+                             String(self.get_ssh_public_key()),
                              String(data), mac))
 
             return (b'-----BEGIN OPENSSH PRIVATE KEY-----\n' +
@@ -318,11 +322,11 @@ class SSHKey:
 
             return data
         elif format_name == 'openssh':
-            data = String(self.algorithm) + self.encode_ssh_public()
+            data = self.get_ssh_public_key()
 
             return self.algorithm + b' ' + binascii.b2a_base64(data)
         elif format_name == 'rfc4716':
-            data = String(self.algorithm) + self.encode_ssh_public()
+            data = self.get_ssh_public_key()
 
             return (b'---- BEGIN SSH2 PUBLIC KEY ----\n' +
                     _wrap_base64(data) +