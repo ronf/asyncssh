87cb30f34ceb2c1495b56b8da94da93270376517
Ron Frederick <ronf@timeheart.net>
2020-12-19 22:56:22 -0800

    Update copyright to 2020

--- asyncssh/editor.py
+++ asyncssh/editor.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2018 by Ron Frederick <ronf@timeheart.net> and others.
+# Copyright (c) 2016-2020 by Ron Frederick <ronf@timeheart.net> and others.
 #
 # This program and the accompanying materials are made available under
 # the terms of the Eclipse Public License v2.0 which accompanies this
