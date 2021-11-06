ronf/asyncssh

commit 16becd851f5ca413e5033f17512a962e9a1e9581

Allow passphrase to be used to load keys used for SSH tunnels
This commit allows a passphrase specified to decrypt keys to be used
on connections through bastion hosts in addition to the main host. This
applies when using the ProxyJump config setting  as well as when using
the "tunnel" argument to specify a host and port to tunnel through.
Thanks go to Wout Megchelenbrink for reporting that this wasn't working
properly!

asyncssh/connection.py
@@ -240,7 +240,7 @@ async def _open_proxy(loop, command, conn_factory):
     return tunnel.get_owner()
 
 
-async def _open_tunnel(tunnel):
+async def _open_tunnel(tunnel, passphrase):
     """Parse and open connection to tunnel over"""
 
     if isinstance(tunnel, str):
@@ -255,12 +255,13 @@ async def _open_tunnel(tunnel):
         else:
             port = ()
 
-        return await connect(host, port, username=username)
+        return await connect(host, port, username=username,
+                             passphrase=passphrase)
     else:
         return None
 
 
-async def _connect(options, loop, flags, conn_factory, msg):
+async def _connect(options, passphrase, loop, flags, conn_factory, msg):
     """Make outbound TCP or SSH tunneled connection"""
 
     host = options.host
@@ -271,7 +272,7 @@ async def _connect(options, loop, flags, conn_factory, msg):
     proxy_command = options.proxy_command
     free_conn = True
 
-    new_tunnel = await _open_tunnel(tunnel)
+    new_tunnel = await _open_tunnel(tunnel, passphrase)
 
     if new_tunnel:
         new_tunnel.logger.info('%s %s via %s', msg, (host, port), tunnel)
@@ -313,8 +314,8 @@ async def _connect(options, loop, flags, conn_factory, msg):
             await conn.wait_closed()
 
 
-async def _listen(options, loop, flags, backlog, reuse_address,
-                  reuse_port, conn_factory, msg):
+async def _listen(options, passphrase, loop, flags, backlog,
+                  reuse_address, reuse_port, conn_factory, msg):
     """Make inbound TCP or SSH tunneled listener"""
 
     def tunnel_factory(_orig_host, _orig_port):
@@ -327,7 +328,7 @@ async def _listen(options, loop, flags, backlog, reuse_address,
     tunnel = options.tunnel
     family = options.family
 
-    new_tunnel = await _open_tunnel(tunnel)
+    new_tunnel = await _open_tunnel(tunnel, passphrase)
 
     if new_tunnel:
         new_tunnel.logger.info('%s %s via %s', msg, (host, port), tunnel)
@@ -6766,7 +6767,8 @@ class SSHServerConnectionOptions(SSHConnectionOptions):
 
 @async_context_manager
 async def connect(host, port=(), *, tunnel=(), family=(), flags=0,
-                  local_addr=None, config=(), options=None, **kwargs):
+                  local_addr=None, config=(), options=None,
+                  passphrase=None, **kwargs):
     """Make an SSH client connection
 
        This function is a coroutine which can be run to create an outbound SSH
@@ -6852,13 +6854,14 @@ async def connect(host, port=(), *, tunnel=(), family=(), flags=0,
                                          family=family, local_addr=local_addr,
                                          **kwargs)
 
-    return await _connect(options, loop, flags, conn_factory,
+    return await _connect(options, passphrase, loop, flags, conn_factory,
                           'Opening SSH connection to')
 
 
 @async_context_manager
 async def connect_reverse(host, port=(), *, tunnel=(), family=(), flags=0,
-                          local_addr=None, config=(), options=None, **kwargs):
+                          local_addr=None, config=(), options=None,
+                          passphrase=None, **kwargs):
     """Create a reverse direction SSH connection
 
        This function is a coroutine which behaves similar to :func:`connect`,
@@ -6928,7 +6931,7 @@ async def connect_reverse(host, port=(), *, tunnel=(), family=(), flags=0,
                                          family=family, local_addr=local_addr,
                                          **kwargs)
 
-    return await _connect(options, loop, flags, conn_factory,
+    return await _connect(options, passphrase, loop, flags, conn_factory,
                           'Opening reverse SSH connection to')
 
 
@@ -6936,7 +6939,7 @@ async def connect_reverse(host, port=(), *, tunnel=(), family=(), flags=0,
 async def listen(host='', port=(), tunnel=(), family=(),
                  flags=socket.AI_PASSIVE, backlog=100, reuse_address=None,
                  reuse_port=None, acceptor=None, error_handler=None,
-                 config=(), options=None, **kwargs):
+                 config=(), options=None, passphrase=None, **kwargs):
     """Start an SSH server
 
        This function is a coroutine which can be run to create an SSH server
@@ -7026,8 +7029,9 @@ async def listen(host='', port=(), tunnel=(), family=(),
     # pylint: disable=attribute-defined-outside-init
     options.proxy_command = None
 
-    return await _listen(options, loop, flags, backlog, reuse_address,
-                         reuse_port, conn_factory, 'Creating SSH listener on')
+    return await _listen(options, passphrase, loop, flags, backlog,
+                         reuse_address, reuse_port, conn_factory,
+                         'Creating SSH listener on')
 
 
 @async_context_manager
@@ -7035,7 +7039,7 @@ async def listen_reverse(host='', port=(), *, tunnel=(), family=(),
                          flags=socket.AI_PASSIVE, backlog=100,
                          reuse_address=None, reuse_port=None,
                          acceptor=None, error_handler=None, config=(),
-                         options=None, **kwargs):
+                         options=None, passphrase=None, **kwargs):
     """Create a reverse-direction SSH listener
 
        This function is a coroutine which behaves similar to :func:`listen`,
@@ -7137,8 +7141,8 @@ async def listen_reverse(host='', port=(), *, tunnel=(), family=(),
     # pylint: disable=attribute-defined-outside-init
     options.proxy_command = None
 
-    return await _listen(options, loop, flags, backlog, reuse_address,
-                         reuse_port, conn_factory,
+    return await _listen(options, passphrase, loop, flags, backlog,
+                         reuse_address, reuse_port, conn_factory,
                          'Creating reverse direction SSH listener on')
 
 
@@ -7183,7 +7187,7 @@ async def get_server_host_key(host, port=(), *, tunnel=(), proxy_command=(),
                               family=(), flags=0, local_addr=None,
                               client_version=(), kex_algs=(),
                               server_host_key_algs=(), config=(),
-                              options=None):
+                              options=None, passphrase=None):
     """Retrieve an SSH server's host key
 
        This is a coroutine which can be run to connect to an SSH server and
@@ -7283,7 +7287,7 @@ async def get_server_host_key(host, port=(), *, tunnel=(), proxy_command=(),
         x509_purposes='any', gss_host=None, kex_algs=kex_algs,
         client_version=client_version)
 
-    conn = await _connect(options, loop, flags, conn_factory,
+    conn = await _connect(options, passphrase, loop, flags, conn_factory,
                           'Fetching server host key from')
 
     server_host_key = conn.get_server_host_key()

commit 4976307cb286ece934381ff86c85ff4e47fc4e13

Rework support for providing passphrase when opening tunnel
This commit reworks commit 16becd85, which accidentally broke passphrase
support on the primary connect function. This new implementation is
simpler and should properly allow the passphrase to be used for both
primary connections and a tunnel when that's not an already open
SSHClientConnection.

Note that other config options passed as arguments will not apply when
opening the tunnel, but any settings coming from the default config file
location will be applied when opening tunnels.

If you need more control over the options used when opening a tunnel
such as specifying different sets of keys or a different passphrase,
you should open the tunnel explicitly first and then pass the resulting
SSHClientConnection option as the tunnel argument.

This commit also includes a new set of unit tests which test passing
encrypted client keys with and without a passphrase (and with an invalid
passphrase) for both the primary connection and for tunnels.

asyncssh/connection.py
@@ -261,7 +261,7 @@ async def _open_tunnel(tunnel, passphrase):
         return None
 
 
-async def _connect(options, passphrase, loop, flags, conn_factory, msg):
+async def _connect(options, loop, flags, conn_factory, msg):
     """Make outbound TCP or SSH tunneled connection"""
 
     host = options.host
@@ -272,7 +272,7 @@ async def _connect(options, passphrase, loop, flags, conn_factory, msg):
     proxy_command = options.proxy_command
     free_conn = True
 
-    new_tunnel = await _open_tunnel(tunnel, passphrase)
+    new_tunnel = await _open_tunnel(tunnel, options.passphrase)
 
     if new_tunnel:
         new_tunnel.logger.info('%s %s via %s', msg, (host, port), tunnel)
@@ -313,8 +313,8 @@ async def _connect(options, passphrase, loop, flags, conn_factory, msg):
             await conn.wait_closed()
 
 
-async def _listen(options, passphrase, loop, flags, backlog,
-                  reuse_address, reuse_port, conn_factory, msg):
+async def _listen(options, loop, flags, backlog, reuse_address,
+                  reuse_port, conn_factory, msg):
     """Make inbound TCP or SSH tunneled listener"""
 
     def tunnel_factory(_orig_host, _orig_port):
@@ -327,7 +327,7 @@ async def _listen(options, passphrase, loop, flags, backlog,
     tunnel = options.tunnel
     family = options.family
 
-    new_tunnel = await _open_tunnel(tunnel, passphrase)
+    new_tunnel = await _open_tunnel(tunnel, options.passphrase)
 
     if new_tunnel:
         new_tunnel.logger.info('%s %s via %s', msg, (host, port), tunnel)
@@ -5597,12 +5597,12 @@ class SSHConnectionOptions(Options):
 
     # pylint: disable=arguments-differ
     def prepare(self, config, protocol_factory, version, host, port, tunnel,
-                proxy_command, family, local_addr, tcp_keepalive, kex_algs,
-                encryption_algs, mac_algs, compression_algs, signature_algs,
-                host_based_auth, public_key_auth, kbdint_auth, password_auth,
-                x509_trusted_certs, x509_trusted_cert_paths, x509_purposes,
-                rekey_bytes, rekey_seconds, connect_timeout, login_timeout,
-                keepalive_interval, keepalive_count_max):
+                passphrase, proxy_command, family, local_addr, tcp_keepalive,
+                kex_algs, encryption_algs, mac_algs, compression_algs,
+                signature_algs, host_based_auth, public_key_auth, kbdint_auth,
+                password_auth, x509_trusted_certs, x509_trusted_cert_paths,
+                x509_purposes, rekey_bytes, rekey_seconds, connect_timeout,
+                login_timeout, keepalive_interval, keepalive_count_max):
         """Prepare common connection configuration options"""
 
         self.config = config
@@ -5613,6 +5613,7 @@ class SSHConnectionOptions(Options):
         self.port = port if port != () else config.get('Port', DEFAULT_PORT)
 
         self.tunnel = tunnel if tunnel != () else config.get('ProxyJump')
+        self.passphrase = passphrase
 
         if isinstance(proxy_command, str):
             proxy_command = shlex.split(proxy_command)
@@ -6211,14 +6212,14 @@ class SSHClientConnectionOptions(SSHConnectionOptions):
                                              _DEFAULT_KEEPALIVE_COUNT_MAX)
 
         super().prepare(config, client_factory or SSHClient, client_version,
-                        host, port, tunnel, proxy_command, family, local_addr,
-                        tcp_keepalive, kex_algs, encryption_algs, mac_algs,
-                        compression_algs, signature_algs, host_based_auth,
-                        public_key_auth, kbdint_auth, password_auth,
-                        x509_trusted_certs, x509_trusted_cert_paths,
-                        x509_purposes, rekey_bytes, rekey_seconds,
-                        connect_timeout, login_timeout, keepalive_interval,
-                        keepalive_count_max)
+                        host, port, tunnel, passphrase, proxy_command, family,
+                        local_addr, tcp_keepalive, kex_algs, encryption_algs,
+                        mac_algs, compression_algs, signature_algs,
+                        host_based_auth, public_key_auth, kbdint_auth,
+                        password_auth, x509_trusted_certs,
+                        x509_trusted_cert_paths, x509_purposes, rekey_bytes,
+                        rekey_seconds, connect_timeout, login_timeout,
+                        keepalive_interval, keepalive_count_max)
 
         if known_hosts == ():
             known_hosts = (config.get('UserKnownHostsFile', []) + \
@@ -6714,14 +6715,14 @@ class SSHServerConnectionOptions(SSHConnectionOptions):
                                              _DEFAULT_KEEPALIVE_COUNT_MAX)
 
         super().prepare(config, server_factory or SSHServer, server_version,
-                        host, port, tunnel, proxy_command, family, local_addr,
-                        tcp_keepalive, kex_algs, encryption_algs, mac_algs,
-                        compression_algs, signature_algs, host_based_auth,
-                        public_key_auth, kbdint_auth, password_auth,
-                        x509_trusted_certs, x509_trusted_cert_paths,
-                        x509_purposes, rekey_bytes, rekey_seconds,
-                        connect_timeout, login_timeout, keepalive_interval,
-                        keepalive_count_max)
+                        host, port, tunnel, passphrase, proxy_command, family,
+                        local_addr, tcp_keepalive, kex_algs, encryption_algs,
+                        mac_algs, compression_algs, signature_algs,
+                        host_based_auth, public_key_auth, kbdint_auth,
+                        password_auth, x509_trusted_certs,
+                        x509_trusted_cert_paths, x509_purposes,
+                        rekey_bytes, rekey_seconds, connect_timeout,
+                        login_timeout, keepalive_interval, keepalive_count_max)
 
         if server_host_keys == ():
             server_host_keys = config.get('HostKey')
@@ -6802,8 +6803,7 @@ class SSHServerConnectionOptions(SSHConnectionOptions):
 
 @async_context_manager
 async def connect(host, port=(), *, tunnel=(), family=(), flags=0,
-                  local_addr=None, config=(), options=None,
-                  passphrase=None, **kwargs):
+                  local_addr=None, config=(), options=None, **kwargs):
     """Make an SSH client connection
 
        This function is a coroutine which can be run to create an outbound SSH
@@ -6890,15 +6890,14 @@ async def connect(host, port=(), *, tunnel=(), family=(), flags=0,
                                          **kwargs)
 
     return await asyncio.wait_for(
-        _connect(options, passphrase, loop, flags, conn_factory,
+        _connect(options, loop, flags, conn_factory,
                  'Opening SSH connection to'),
         timeout=options.connect_timeout)
 
 
 @async_context_manager
 async def connect_reverse(host, port=(), *, tunnel=(), family=(), flags=0,
-                          local_addr=None, config=(), options=None,
-                          passphrase=None, **kwargs):
+                          local_addr=None, config=(), options=None, **kwargs):
     """Create a reverse direction SSH connection
 
        This function is a coroutine which behaves similar to :func:`connect`,
@@ -6969,7 +6968,7 @@ async def connect_reverse(host, port=(), *, tunnel=(), family=(), flags=0,
                                          **kwargs)
 
     return await asyncio.wait_for(
-        _connect(options, passphrase, loop, flags, conn_factory,
+        _connect(options, loop, flags, conn_factory,
                  'Opening reverse SSH connection to'),
         timeout=options.connect_timeout)
 
@@ -6978,7 +6977,7 @@ async def connect_reverse(host, port=(), *, tunnel=(), family=(), flags=0,
 async def listen(host='', port=(), tunnel=(), family=(),
                  flags=socket.AI_PASSIVE, backlog=100, reuse_address=None,
                  reuse_port=None, acceptor=None, error_handler=None,
-                 config=(), options=None, passphrase=None, **kwargs):
+                 config=(), options=None, **kwargs):
     """Start an SSH server
 
        This function is a coroutine which can be run to create an SSH server
@@ -7069,7 +7068,7 @@ async def listen(host='', port=(), tunnel=(), family=(),
     options.proxy_command = None
 
     return await asyncio.wait_for(
-        _listen(options, passphrase, loop, flags, backlog, reuse_address,
+        _listen(options, loop, flags, backlog, reuse_address,
                 reuse_port, conn_factory, 'Creating SSH listener on'),
         timeout=options.connect_timeout)
 
@@ -7079,7 +7078,7 @@ async def listen_reverse(host='', port=(), *, tunnel=(), family=(),
                          flags=socket.AI_PASSIVE, backlog=100,
                          reuse_address=None, reuse_port=None,
                          acceptor=None, error_handler=None, config=(),
-                         options=None, passphrase=None, **kwargs):
+                         options=None, **kwargs):
     """Create a reverse-direction SSH listener
 
        This function is a coroutine which behaves similar to :func:`listen`,
@@ -7182,7 +7181,7 @@ async def listen_reverse(host='', port=(), *, tunnel=(), family=(),
     options.proxy_command = None
 
     return await asyncio.wait_for(
-        _listen(options, passphrase, loop, flags, backlog,
+        _listen(options, loop, flags, backlog,
                 reuse_address, reuse_port, conn_factory,
                 'Creating reverse direction SSH listener on'),
         timeout=options.connect_timeout)
@@ -7229,7 +7228,7 @@ async def get_server_host_key(host, port=(), *, tunnel=(), proxy_command=(),
                               family=(), flags=0, local_addr=None,
                               client_version=(), kex_algs=(),
                               server_host_key_algs=(), config=(),
-                              options=None, passphrase=None):
+                              options=None):
     """Retrieve an SSH server's host key
 
        This is a coroutine which can be run to connect to an SSH server and
@@ -7330,7 +7329,7 @@ async def get_server_host_key(host, port=(), *, tunnel=(), proxy_command=(),
         client_version=client_version)
 
     conn = await asyncio.wait_for(
-        _connect(options, passphrase, loop, flags, conn_factory,
+        _connect(options, loop, flags, conn_factory,
                  'Fetching server host key from'),
         timeout=options.connect_timeout)
 

tests/test_connection_auth.py
@@ -1011,6 +1011,29 @@ class _TestPublicKeyAuth(ServerTestCase):
         return conn
 
     @asynctest
+    async def test_encrypted_client_key(self):
+        """Test public key auth with encrypted client key"""
+
+        async with self.connect(username='ckey', client_keys='ckey_encrypted',
+                                passphrase='passphrase'):
+            pass
+
+    @asynctest
+    async def test_encrypted_client_key_bad_passphrase(self):
+        """Test wrong passphrase for encrypted client key"""
+
+        with self.assertRaises(asyncssh.KeyEncryptionError):
+            await self.connect(username='ckey', client_keys='ckey_encrypted',
+                               passphrase='xxx')
+
+    @asynctest
+    async def test_encrypted_client_key_missing_passphrase(self):
+        """Test missing passphrase for encrypted client key"""
+
+        with self.assertRaises(asyncssh.KeyImportError):
+            await self.connect(username='ckey', client_keys='ckey_encrypted')
+
+    @asynctest
     async def test_client_certs(self):
         """Test trusted client certificate via client_certs"""
 

tests/test_forward.py
@@ -30,7 +30,7 @@ import unittest
 from unittest.mock import patch
 
 import asyncssh
-from asyncssh.misc import maybe_wait_closed
+from asyncssh.misc import maybe_wait_closed, write_file
 from asyncssh.packet import String, UInt32
 from asyncssh.public_key import CERT_TYPE_USER
 from asyncssh.socks import SOCKS5, SOCKS5_AUTH_NONE
@@ -309,6 +309,61 @@ class _TestTCPForwarding(_CheckForwarding):
             await asyncssh.connect('0.0.0.1',
                                    tunnel='%s:%d' % (self._server_addr,
                                                      self._server_port))
+
+    @asynctest
+    async def test_proxy_jump(self):
+        """Test connecting a tunnneled SSH connection using ProxyJump"""
+
+        write_file('.ssh/config', 'Host target\n'
+                   '  Hostname localhost\n'
+                   f'  Port {self._server_port}\n'
+                   f'  ProxyJump localhost:{self._server_port}\n'
+                   'IdentityFile ckey\n', 'w')
+
+        try:
+            async with self.connect(host='target', username='ckey'):
+                pass
+        finally:
+            os.remove('.ssh/config')
+
+    @asynctest
+    async def test_proxy_jump_encrypted_key(self):
+        """Test ProxyJump with encrypted client key"""
+
+        write_file('.ssh/config', 'Host *\n'
+                   '  User ckey\n'
+                   'Host target\n'
+                   '  Hostname localhost\n'
+                   f'  Port {self._server_port}\n'
+                   f'  ProxyJump localhost:{self._server_port}\n'
+                   '  IdentityFile ckey_encrypted\n', 'w')
+
+        try:
+            async with self.connect(host='target', username='ckey',
+                                    client_keys='ckey_encrypted',
+                                    passphrase='passphrase'):
+                pass
+        finally:
+            os.remove('.ssh/config')
+
+    @asynctest
+    async def test_proxy_jump_encrypted_key_missing_passphrase(self):
+        """Test ProxyJump with encrypted client key and missing passphrase"""
+
+        write_file('.ssh/config', 'Host *\n'
+                   '  User ckey\n'
+                   'Host target\n'
+                   '  Hostname localhost\n'
+                   f'  Port {self._server_port}\n'
+                   f'  ProxyJump localhost:{self._server_port}\n'
+                   '  IdentityFile ckey_encrypted\n', 'w')
+
+        try:
+            with self.assertRaises(asyncssh.KeyImportError):
+                await self.connect(host='target', username='ckey',
+                                   client_keys='ckey_encrypted')
+        finally:
+            os.remove('.ssh/config')
 
     @asynctest
     async def test_ssh_connect_reverse_tunnel(self):