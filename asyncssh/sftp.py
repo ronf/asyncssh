ce5b41abe30b399d86af1d04a18d092617cda23c
Ron Frederick <ronf@timeheart.net>
2022-01-26 07:10:05 -0800

    Update return types for LocalFS and SFTPServerFS open() methods
    Mirroring the change in commit a4b2615, this commit updates the return
    types in the open() call of the LocalFS and SFTPServerFS classes to
    reflect the actual class they return rather than a protocol they
    were created to follow.
    
    This commit also fixes the async context manager methods in these
    classes. While currently unused, the methods were defined to allow their
    signature to match the open() method in SFTPClient. This change makes
    sure that the async context manager funcitonality of these classes will
    work correctly if it is ever used.

--- asyncssh/sftp.py
+++ asyncssh/sftp.py
@@ -7103,13 +7103,19 @@ class LocalFile:
     def __init__(self, file: _SFTPFileObj):
         self._file = file
 
-    async def __aenter__(self) -> 'SFTPFileProtocol':
+    async def __aenter__(self) -> 'LocalFile': # pragma: no cover
         """Allow LocalFile to be used as an async context manager"""
+
+        return self
 
     async def __aexit__(self, _exc_type: Optional[Type[BaseException]],
                         _exc_value: Optional[BaseException],
-                        _traceback: Optional[TracebackType]) -> bool:
+                        _traceback: Optional[TracebackType]) -> \
+            bool: # pragma: no cover
         """Wait for file close when used as an async context manager"""
+
+        await self.close()
+        return False
 
     async def read(self, size: int, offset: int) -> bytes:
         """Read data from the local file"""
@@ -7212,7 +7218,7 @@ class LocalFS:
         os.symlink(_to_local_path(oldpath), _to_local_path(newpath))
 
     @async_context_manager
-    async def open(self, path: bytes, mode: str) -> SFTPFileProtocol:
+    async def open(self, path: bytes, mode: str) -> LocalFile:
         """Open a local file"""
 
         # pylint: disable=unused-argument
@@ -7240,6 +7246,7 @@ class SFTPServerFile:
             bool: # pragma: no cover
         """Wait for client close when used as an async context manager"""
 
+        await self.close()
         return False
 
     async def read(self, size: int, offset: int) -> bytes:
@@ -7357,7 +7364,7 @@ class SFTPServerFS:
             await result
 
     @async_context_manager
-    async def open(self, path: bytes, mode: str) -> SFTPFileProtocol:
+    async def open(self, path: bytes, mode: str) -> SFTPServerFile:
         """Open a file"""
 
         pflags, _ = _mode_to_pflags(mode)
