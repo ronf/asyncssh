87ef61393535f848858d284532800dc31dc55fc0
Ron Frederick <ronf@timeheart.net>
2022-07-09 11:18:09 -0700

    Fix import order to make pylint happy

--- tests/sspi_stub.py
+++ tests/sspi_stub.py
@@ -24,11 +24,11 @@
 
 import sys
 
+from .gss_stub import step
+
 if sys.platform == 'win32': # pragma: no cover
     from asyncssh.gss_win32 import ASC_RET_INTEGRITY, ISC_RET_INTEGRITY
     from asyncssh.gss_win32 import SECPKG_ATTR_NATIVE_NAMES, SSPIError
-
-from .gss_stub import step
 
 
 class SSPIBuffer:
