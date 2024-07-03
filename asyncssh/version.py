6d1a99fe68bf5e85a846d327add44d781ec21fed
Ron Frederick <ronf@timeheart.net>
2024-07-03 15:10:14 -0700

    Bump version number up to 2.15.0 and update change log

--- asyncssh/version.py
+++ asyncssh/version.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013-2023 by Ron Frederick <ronf@timeheart.net> and others.
+# Copyright (c) 2013-2024 by Ron Frederick <ronf@timeheart.net> and others.
 #
 # This program and the accompanying materials are made available under
 # the terms of the Eclipse Public License v2.0 which accompanies this
@@ -26,4 +26,4 @@ __author_email__ = 'ronf@timeheart.net'
 
 __url__ = 'http://asyncssh.timeheart.net'
 
-__version__ = '2.14.2'
+__version__ = '2.15.0'
